8a6b2b99b672025bb62bc7dd433fd98123912178
vcarreno96 <vcarreno96@users.noreply.github.com>
2015-04-20 13:27:28 +0000

    added project files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app = express();
+
+//app.use(express.static('angular-seed/app'));
+  app.use(express.static('/'));
+        app.use(express.static(__dirname + '/sb-admin-angular/app'));
+
+    
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000,process.env.IP, function(){
+       console.log('example app listening  on :%s', 3000);        
+});
