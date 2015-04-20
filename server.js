3cc11efb2956508745d0256ac4fdc0b2f2fe1667
vcarreno96 <vcarreno96@users.noreply.github.com>
2015-04-20 18:42:24 +0000

    cleanup

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var express = require('express'),
     app = express();
 
-//app.use(express.static('angular-seed/app'));
-  app.use(express.static('/'));
-        app.use(express.static(__dirname + '/public/app'));
-
-    
-app.get('/', function(req, res){
-    res.send('Hello World!');
-});
+app.use(express.static('/'));
+app.use(express.static(__dirname + '/public/app'));
 
 var server = app.listen(3000,process.env.IP, function(){
        console.log('example app listening  on :%s', 3000);        
